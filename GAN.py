a1cb537ec841287184e8a79288a2297a86942e3f
A-mockingbird <littlebirdAI@126.com>
2019-04-21 08:29:13 +0800

    Enter

---
+++ GAN.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+class GAN(object):
+    def init(self, sess):
+        self.sess = sess
+        self.imdb = imdb
+        self.names_class = names_class
+    
+    def createGAN(self, config):
+        
