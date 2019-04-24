3e2c82693231f4623325b9b1d1e1d08971a7675b
A-mockingbird <littlebirdAI@126.com>
2019-04-24 17:22:20 +0800

    yes

--- GAN.py
+++ GAN.py
@@ -9,13 +9,16 @@ class GAN(object):
         
     def createGAN(self, config):
         self.number_classes = number_classes
-        self.hidden_layer1 = hidden_layer1
-        self.hidden_layer2 = hidden_layer2
+        self.g_hidden_layer1 = g_hidden_layer1
+        self.g_hidden_layer2 = g_hidden_layer2
+        self.a_hidden_layer1 = a_hidden_layer1
+        self.a_hidden_layer2 = a_hidden_layer2
         self.input_images = tf.placeholder(tf.float32, [None, self.input_size])
         self.keep = tf.placeholder(tf.float32)
-        g_images = _build_generate_network(self.input_images)
-    
-    def _build_generate_network(self, g_input):
+        g_images = _Build_GenerativeNetwork(self.input_images)
+        probabilities = _Build_AdversarialNetwork(a_input)
+
+    def _Build_GenerativeNetwork(self, g_input):
         generate_weights = _initialize_generate_variables()
         layer0 = tf.nn.relu(tf.matmul(g_input, generate_weights['w0']) + generate_weights['b0'])
         drop_layer0 = tf.nn.dropout(layer0, self.keep)
@@ -27,16 +30,31 @@ class GAN(object):
 
     def _initialize_generate_variables(self):
         generate_weights = dict{}
-        w0 = tf.Variable(tf.truncated_normal([self.input_size, self.hidden_layer1], stddev=0.1))
+        w0 = tf.Variable(tf.truncated_normal([self.input_size, self.g_hidden_layer1], stddev=0.1))
         generate_weights['w0'] = w0
-        b0 = tf.Variable(tf.zeros([self.hidden_layer1]))
+        b0 = tf.Variable(tf.zeros([self.g_hidden_layer1]))
         generate_weights['b0'] = b0
-        w1 = tf.Variable(tf.zeros([self.hidden_layer1], self.hidden_layer2))
+        w1 = tf.Variable(tf.zeros([self.g_hidden_layer1], self.g_hidden_layer2))
         generate_weights['w1'] = w1
-        b1 = tf.Variable(tf.zeros([self.hidden_layer2]))
+        b1 = tf.Variable(tf.zeros([self.g_hidden_layer2]))
         generate_weights['b1'] = b1
-        w2 = tf.Variable(tf.zeros([self.hidden_layer2, self.input_images]))
+        w2 = tf.Variable(tf.zeros([self.g_hidden_layer2, self.input_images]))
         generate_weights['w2'] = w2
         b2 = tf.Variable(tf.zeros([self.input_images]))
         generate_weights['b2'] = b2
         return generate_weights
+    
+    def _Build_AdversarialNetwork(self):
+        adversary_weights = _initialize_adversary_variables()
+
+    def _initialize_adversary_variables(self):
+        adversary_weights = dict{}
+        w0 = tf.Variable(tf.truncated_normal([self.input_size, self.a_hidden_layer1], stddev=0.1))
+        adversary_weights['w0'] = w0
+        b0 = tf.Variable(tf.zeros([self.a_hidden_layer1]))
+        adversary_weights['b0'] = b0
+        w1 = tf.Variable(tf.zeros([self.a_hidden_layer1, self.a_hidden_layer2]))
+        adversary_weights['w1'] = w1
+        b1 = tf.Variable(tf.zeros([self.a_hidden_layer2]))
+        adversary_weights['b1'] = b1
+        
